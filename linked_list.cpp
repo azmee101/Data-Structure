f30e39ba51825d74bba2a065d451b1c7d031cbc2
Mohammad Azmee Tofail <iazmee101@gmail.com>
2025-03-24 00:24:56 +0600

    interview

--- linked_list.cpp
+++ linked_list.cpp
@@ -17,7 +17,7 @@ void insert(int data, Node*& head, Node*& tail){
     Node* newNode = new Node{data, nullptr};
     if(head == nullptr){
         head = newNode;
-        tail = head; // this line will need if I want to add node form last
+        tail = head;
         return;
     }
     Node* tmp = head;
@@ -25,7 +25,7 @@ void insert(int data, Node*& head, Node*& tail){
         tmp = tmp->next;
     }
     tmp->next = newNode;
-    tail = tmp->next; // this line will need if I want to add node form last
+    tail = tmp->next;
     return;
 }
 
@@ -34,6 +34,7 @@ void insert_last(Node*& lastNode, int data, Node*& tail){
     tail->next = lastNode;
     return;
 }
+
 
 void display(Node*& head){
     Node* tmp = head;
@@ -44,19 +45,57 @@ void display(Node*& head){
     cout << "null" << '\n';
 }
 
+
+int count(Node*& head){
+    int cnt = 1;
+    Node* tmp = head;
+    while(tmp->next != nullptr){
+        cnt++;
+        tmp = tmp->next;
+    }
+    return cnt;
+}
+
+bool search(int val, Node*& head){
+    Node* tmp = head;
+    while(tmp->next != nullptr){
+        if(tmp->data == val){
+            return true;
+        }
+        tmp = tmp->next;
+    }
+    return false;
+}
+
+
 int main()
 {
     Node* head = nullptr;
     Node* tail = nullptr;
+    
     insert(10, head, tail);
     insert(20, head, tail);
     insert(30, head, tail);
+    
     display(head);
+    
     Node* firstNode = nullptr;
     insert_first(firstNode, 50, head);
+    
     display(head);
+    
     Node* lastNode = nullptr;
     insert_last(lastNode, 14, tail);
+    
     display(head);
+    
+    cout << count(head) << '\n';
+    
+    if(search(20, head)){
+        cout << "Exist!\n";
+    }
+    else{
+        cout << "Not Exist\n";
+    }
     return;
 }
